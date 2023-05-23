845cd6f92919c4205256b9f1d94e4dfa52f54177
KrishnaMohan123-pro <krishnamohan.bitsindri@gmail.com>
2023-05-24 00:11:48 +0530

    feature: add redux store

--- store/index.js
+++ store/index.js
@@ -1,7 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productReducer from "./products";
+import { configureStore } from '@reduxjs/toolkit';
+import productReducer from './products';
+import { useDispatch, useSelector } from 'react-redux';
 const store = configureStore({
     reducer: { products: productReducer },
 });
+
+export const useAppDispatch = useDispatch;
+export const useAppSelector = useSelector;
 
 export { store };
