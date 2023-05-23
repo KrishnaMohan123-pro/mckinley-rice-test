bdd9360d6e0efc30b99afe2ac6df817f2736fdaa
KrishnaMohan123-pro <krishnamohan.bitsindri@gmail.com>
2023-05-24 00:12:37 +0530

    feature: add home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,67 @@
-import React from "react";
-import Card from "../components/Card/Card";
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProducts } from '../store/products';
+import { Loader } from '../components/Loader/Loader';
+import Card from '../components/Card/Card';
 
 const index = () => {
+    const { isLoading, products } = useSelector((state) => state).products;
+    const [page, setPage] = useState(1);
+    const observer = useRef();
+    const dispatch = useDispatch();
+    const lastPageRef = useCallback(
+        (node) => {
+            if (page < 4) {
+                if (isLoading) return;
+                if (observer.current) observer.current.disconnect();
+                observer.current = new IntersectionObserver((entries) => {
+                    if (entries[0].isIntersecting) {
+                        setPage((prev) => prev + 1);
+                    }
+                });
+                if (node) observer.current.observe(node);
+            }
+        },
+        [isLoading]
+    );
+
+    useEffect(() => {
+        dispatch(getProducts({ pageNumber: page }));
+    }, [dispatch, page]);
+    // console.log(state.products);
+    if (isLoading && products.length === 0)
+        return (
+            <div className='container mx-auto pt-7 text-center'>
+                <Loader />
+            </div>
+        );
     return (
-        <div className="container mx-auto">
-            <Card />
-        </div>
+        <>
+            <div className='container mx-auto pt-7 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+                {products.map((p, i) => {
+                    if (products.length === i + 1)
+                        return (
+                            <div
+                                key={p.id}
+                                ref={lastPageRef}
+                                className='mx-auto'
+                            >
+                                <Card product={p} />
+                            </div>
+                        );
+                    return (
+                        <div key={p.id} className='mx-auto'>
+                            <Card product={p} />
+                        </div>
+                    );
+                })}
+            </div>
+            {isLoading ? (
+                <div className='container mx-auto pt-7 text-center'>
+                    <Loader />
+                </div>
+            ) : null}
+        </>
     );
 };
 
