691f046eeaeda72ae381f2ff54116e1ffbec2534
KrishnaMohan123-pro <krishnamohan.bitsindri@gmail.com>
2023-05-24 00:09:39 +0530

    feature: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {},
         container: {
             padding: {
-                DEFAULT: "1rem",
-                sm: "2rem",
-                lg: "4rem",
-                xl: "5rem",
-                "2xl": "6rem",
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
             },
+        },
+        colors: {
+            grey: {
+                primary: '#323438',
+                secondary: '#85878c',
+                other: '#e5e6e9',
+            },
+            white: '#ffffff',
+            green: '#00c362',
+            blue: '#2196f3',
         },
     },
     plugins: [],
