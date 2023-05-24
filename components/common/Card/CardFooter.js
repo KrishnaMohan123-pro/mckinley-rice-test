55fc38291c1897c42eb44d265bde0565627630fe
KrishnaMohan123-pro <krishnamohan.bitsindri@gmail.com>
2023-05-24 16:45:12 +0530

    bugfix: add correct footer font-size

--- components/common/Card/CardFooter.js
+++ components/common/Card/CardFooter.js
@@ -4,7 +4,7 @@ export const CardFooter = (props) => {
     const { reward } = props;
     return (
         <>
-            <div className='pt-2 flex items-center font-bold'>
+            <div className='pt-2 flex items-center font-bold text-sm'>
                 <WonSymbol /> 취업 축하금: {reward}원
             </div>
         </>
